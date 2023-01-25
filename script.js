0fd8f40e70223f3a94c1ea37befb76dc356d885b
AijaRe <aija.repsa@gmail.com>
2023-01-25 21:43:22 +0000

    html css and js for Wild Edibles

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert();
+function addSubscriber() {
+  let name = prompt("What is your name?");
+  let email = prompt("What is your e-mail?");
+  if (name.length > 0 && email.length > 5) {
+    alert(`${name}, welcome to the wild side! 🌱 We´ll be in touch!`);
+  } else {
+    alert(`Please enter valid data. You are welcome to join us any time!💚`);
+  }
+}
+let subscribeButton = document.querySelector(".subscribe-button");
+subscribeButton.addEventListener("click", addSubscriber);
